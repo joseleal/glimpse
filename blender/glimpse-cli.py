d2f01d6df997e5e2ce5e4552e4c5b6956a3690b4
Robert Bragg <robert@impossible.com>
2017-12-13 14:20:02 +0000

    fix: detect when required addon not enabled

--- blender/glimpse-cli.py
+++ blender/glimpse-cli.py
@@ -1,5 +1,25 @@
+# Copyright (c) 2017 Glimp IP Ltd
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # Run like:
-# $ blender.exe -b /path/to/glimpse-training.blend -P generator-cmd.py -- -h
+# $ blender.exe -b /path/to/glimpse-training.blend -P glimpse-cli.py -- --help-glimpse
 
 import os
 import sys
@@ -26,10 +46,12 @@ parser.add_argument('--name', default=os.getcwd(), help='Unique name for this re
 parser.add_argument('mocaps', help='Directory with motion capture files')
 args = parser.parse_args(argv)
 
-glimpse_addon_name = 'Glimpse Training Data Generator'
-
 addon_dependencies = [
-    glimpse_addon_name,
+    'glimpse_data_generator',
+    'mesh_paint_rig',
+    'makeclothes',
+    'maketarget',
+    'makewalk',
 ]
 
 dep_error = ""
